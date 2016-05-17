6358c6a44a3152b35e4c6343daffceadd0b5e909
Edgar23P <edgar_v23@hotmail.com>
2016-05-17 11:09:24 -0500

    17/05bueno

---
+++ app/src/main/java/com/example/ernesto/remembrall/vistaconsulta.java
@@ -0,0 +1,21 @@
+package com.example.ernesto.remembrall;
+
+/**
+ * Created by edgar on 17/05/16.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vistaconsulta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vista_consulta);
+    }
+}
